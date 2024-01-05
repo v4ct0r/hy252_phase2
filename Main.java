1f5ed523f04d03d788265105e57a021e70bc0509
victo <victorsfak03@gmail.com>
2024-01-05 13:28:39 +0200

    ALL DONE

---
+++ Main.java
@@ -0,0 +1,10 @@
+import controller.Controller;
+import view.GameView;
+
+public class Main {
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        GameView game = new GameView(controller);
+       // System.out.print("Hello World!");
+    }
+}
