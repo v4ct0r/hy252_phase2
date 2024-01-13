358b589c57dadcb1766fbc91b99497aa017d6829
VICTORAS SFAKIANAKIS <Victorsfak03@gmail.com>
2024-01-13 14:39:37 +0200

    DONE #4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import controller.Controller;
 import view.GameView;
 
+/**
+ * @author Viktoras Sfakianakis
+ */
 public class Main {
     public static void main(String[] args) {
         GameView game = new GameView(new Controller());
